9459b03c93e330cd42fb7d395720e5b770555af9
Hendro Steven <hendro.steven@gmail.com>
2017-09-01 12:26:48 +0800

    Initial

---
+++ src/main/java/com/kelaskoding/CandidateApp/CandidateAppApplication.java
@@ -0,0 +1,12 @@
+package com.kelaskoding.CandidateApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateAppApplication.class, args);
+	}
+}
